d9f26db8e0ab4f9060d8650dd8e750256beb1af3
Dillon Kearns <dillon@incrementalelm.com>
2024-04-30 10:52:51 -0700

    Update todo chat.

--- scripts/todo-chat.ts
+++ scripts/todo-chat.ts
@@ -14,7 +14,7 @@ import {
   HumanMessagePromptTemplate,
   SystemMessagePromptTemplate,
   MessagesPlaceholder,
-} from "langchain/prompts";
+} from "@langchain/core/prompts";
 
 type Todo = {
   uuid: string;
@@ -33,25 +33,49 @@ type Todo = {
 };
 const commandOutput: string = await $`things-cli --json today`;
 const initialTodos: Todo[] = JSON.parse(commandOutput);
-// const initialTodos = [];
+const completedTodos: Todo[] = JSON.parse(await $`things-cli --json logtoday`);
+
+const todosSection = `
+Here are my todos on my "Today" list:
+    
+${initialTodos.map((item, index) => `${index + 1}. ${item.title}`).join("\n")}
+
+And here are the items I've completed today:
+
+${completedTodos.map((item) => `- ${item.title}`).join("\n")}
+`;
 
 let prompt = ChatPromptTemplate.fromMessages([
   `You are a productivity coach and assistant, and I am your client. You are an expert in the GTD (Getting Things Done) methodology and are helping me follow the GTD principles to be more productive.
+
+You not only have a deep understanding of GTD, but you are empathic, wise, insightful, and a great listener. You know the power of simple questions.
+
+You are versed in the technique "powerful questions" where you can frame a question to allow thinking with a new set of assumptions. You recognize the value of socratic questions where you don't lead to a conclusion, but can still provide helpful insights through helping frame the process while not giving the solutions.
+
+That said, don't be afraid to point out whe you see clear places where something can be imrpoved or clarified.
 
 Some of your common activities to help your clients include:
 
 - Reviewing their todo list to find fuzzy items ("amorphous blobs of undoability"), and suggest ways to turn then into Next Actions (physical, visible, doable, small tasks). You often ask clarifying questions to help break these tasks down.
 - Asking what the most valuable project or outcome for the day is. Based on this lens, help decide what will move this *to completion* (instead of just staying busy but not actually getting across the finish line with important outcomes)
 - Asking what is weighing down your productivity for the day (are your inboxes clear? Do you need to do a brain dump? Do you need to clarify outcomes that are on your mind?)
+- Celebrating wins and progress. Building off of that momentum with encouragement, and reminding of how good it will feel to be completely done with a project (in the case of completing a sub-task of a project).
+
+
+Some example questions that are useful:
+
+- What are your must-dos and key priorities for the day?
+- Which of these tasks can this be completed in 2 minutes or less?
+- Do you feel that you could pick up this action and do it right now?
+- Are any of these tasks repelling you, or have they been ion your list for a long time?
 
 Note that I have ADHD, so breaking down tasks is incredibly helpful for unblocking me on action items. It is also valuable to let go of certain commitments when I realize they are not meaningful, or to simplify commitments and find an 80-20 way to accomplish them that doesn't weigh me down as much.
 
 You have access to my todo list data for my "Today" view, which is items I have set as a priority that I want to accomplish today (from Things 3). You can use this as input for your coaching, as well as asking questions. DO NOT comment about the format of the data. This is just one input to your coaching insights. Instead, you must converse about the items themselves and their attributes, as a helpful and insightful personal assistant or business coach would.
 
-Here are my todos:
-    
-${initialTodos.map((item) => `- ${item.title}`).join("\n")}`,
-  // ${JSON.stringify(initialTodos)}`
+When referring to specific items in the list, use the provided title as well as the index number in the list. That way we can easily refer to items using a shorthand so I can do less typing to answer your questions.
+
+${todosSection}`,
   new MessagesPlaceholder("history"),
   // HumanMessagePromptTemplate.fromTemplate("{input}"),
   ["human", "{input}"],
@@ -66,6 +90,7 @@ let llm = new ChatOllama({
   model: "llama3:70b",
   // model: "llama3",
   numGpu: 99,
+
   // model: "llama3",
   streaming: true,
   callbacks: [
@@ -74,11 +99,12 @@ let llm = new ChatOllama({
         id = setTimeout(() => {
           chat.setMessage(
             -1,
-            md(`### Sorry, the AI is taking a long time to respond.`)
+            md(`### Sorry, the AI is taking a long time to respond.`),
           );
           setLoading(true);
         }, 3000);
         log(`handleLLMStart`);
+        chat.addMessage(md(todosSection));
         currentMessage = ``;
         chat.addMessage("");
       },
@@ -134,8 +160,12 @@ chain.pipe(prompt);
 
 let controller = new AbortController();
 
+const formattedDate = new Date().toLocaleDateString("en-US", {
+  dateStyle: "full",
+});
+
 chain.call({
-  input: "Hello! Do you have any suggestions?",
+  input: `Hello! Today is ${formattedDate}. Do you have any suggestions?`,
   controller,
 });
 
