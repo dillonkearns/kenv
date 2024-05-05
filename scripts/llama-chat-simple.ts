b5f72d10dd762a0f4801ba7c2e4f643bf996d132
Dillon Kearns <dillon@incrementalelm.com>
2024-05-05 16:50:21 -0700

    Support replies in llama conversation widget.

--- scripts/llama-chat-simple.ts
+++ scripts/llama-chat-simple.ts
@@ -6,51 +6,53 @@ import "@johnlindquist/kit";
 import fetch from "node-fetch";
 import * as readline from "readline";
 
+
+let running = true;
+let conversationHistory = [];
+
 await chat({
-  onInit: async () => {
-    chat.addMessage("Why is the sky blue?");
-    chat.addMessage("");
-    // Conversation history
-    const userMessage = "Why is the sky blue?";
-    const conversationHistory = [{ role: "user", content: userMessage }];
+  onInit: async () => {},
+  onSubmit: async (input) => {
+    running = true;
+    sendUserMessage(input);
+  },
+});
 
-    const response = await fetch("http://127.0.0.1:11434/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        model: "llama3:70b",
-        messages: [
-          ...conversationHistory,
-          //  { role: "user", content: userMessage }],
-        ],
-        stream: true,
-        num_gpu: 99,
-      }),
-    });
+async function sendUserMessage(userMessage: string) {
+  chat.addMessage("");
+  conversationHistory.push({ role: "user", content: userMessage });
 
-    let assistantResponse = "";
+  const response = await fetch("http://127.0.0.1:11434/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "llama3:70b",
+      messages: conversationHistory,
+      stream: true,
+      num_gpu: 99,
+    }),
+  });
 
-    const rl = readline.createInterface({
-      input: response.body,
-      crlfDelay: Infinity,
-    });
+  running = false;
 
-    for await (const line of rl) {
-      debugger;
-      if (line.trim() !== "") {
-        const parsed = JSON.parse(line);
-        if (parsed.message && parsed.message.role === "assistant") {
-          assistantResponse += parsed.message.content;
-          chat.setMessage(-1, md(assistantResponse));
-          // Process or display the chunk of the assistant's response
-        }
+  let assistantResponse = "";
+
+  const rl = readline.createInterface({
+    input: response.body,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) {
+    if (line.trim() !== "") {
+      const parsed = JSON.parse(line);
+      if (parsed.message && parsed.message.role === "assistant") {
+        assistantResponse += parsed.message.content;
+        chat.setMessage(-1, md(assistantResponse));
       }
     }
 
-    // Update the conversation history with the user message and assistant response
-    conversationHistory.push({ role: "user", content: userMessage });
     conversationHistory.push({ role: "assistant", content: assistantResponse });
-  },
-});
+  }
+}
